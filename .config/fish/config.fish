0c770e244a5f8db6bfbb014a93828d4e5dca1458
Damien Hubleur <damien.hubleur@coderpad.io>
2024-04-03 15:46:40 +0200

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export LC_ALL=fr_FR.UTF-8
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+set -g fish_vcs_branch_name_length 30
+
---
+++ .config/fish/functions/md.fish
@@ -0,0 +1,3 @@
+function md
+	glow $argv
+end
